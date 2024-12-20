1d326f1afe68f470d4278419187d5b35f23f61f4
NamrataOit28 <namrataOit28@gmail.com>
2024-12-20 11:05:00 +0530

    chnge route

--- routers/user_routers.js
+++ routers/user_routers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router= express.Router();
 const userController = require('../controller/user_controller');
 
-router.post('/register',userController.registerUser);
+router.post('/registerUser',userController.registerUser);
 router.post('/login',userController.loginUser);
 
 module.exports = router;
